c92ff6cc3c62c54e01c35926bfa9f429ee8cb854
plotnikov.ivan10 <plotnikov.ivan10@rwb.ru>
2026-08-01 12:52:40 +0400

    add compressions + turn off refetch tanstack

--- src/app/providers/queryClient.ts
+++ src/app/providers/queryClient.ts
@@ -5,6 +5,8 @@ export const queryClient = new QueryClient({
     queries: {
       retry: 1,
       staleTime: 30_000,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
     },
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import react from '@vitejs/plugin-react'
+import { constants } from 'node:zlib'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import { compression, defineAlgorithm } from 'vite-plugin-compression2'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithms: [
+        defineAlgorithm('brotliCompress', {
+          params: {
+            [constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        }),
+      ],
+      threshold: 1024,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
